1e5e08534b2fd04e663dd425057eb75903a06ce0
David Neumaier <filosofem.dave@gmail.com>
2017-12-31 22:11:18 +0100

    hair conditioner added
    ha ha ha

---
+++ Types/Condition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qrakhen.Sqript
+{
+    public abstract class Condition : Funqtion
+    {
+        public const string
+            CHAR_OPEN = "{",
+            CHAR_CLOSE = "}";
+
+        public Context parent { get; protected set; }
+        public Expression expression { get; protected set; }
+        public Condition elseCondition { get; protected set; }
+
+        public Condition(Context parent) : base(parent) {
+
+        }
+    }
+}
