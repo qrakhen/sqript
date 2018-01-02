c814332e30565b99e067d21eaee9df0935f19f43
David Neumaier <filosofem.dave@gmail.com>
2018-01-02 21:45:50 +0100

    added all operator callbacks, awesome. that's pretty sexy.

--- Core/Runtime.cs
+++ Core/Runtime.cs
@@ -408,21 +408,91 @@ namespace Qrakhen.Sqript
         }
 
         static void defineOperators() {
-            Operators.define(Operator.CALCULATE_ADD, delegate (Value a, Value b) {
-                return new Value(a.getValue<int>() + b.getValue<int>(), ValueType.INTEGER);
-            });
-            Operators.define(Operator.CALCULATE_SUBTRACT);
-            Operators.define(Operator.CALCULATE_DIVIDE);
-            Operators.define(Operator.CALCULATE_MULTIPLY);
             Operators.define(Operator.ASSIGN_VALUE);
             Operators.define(Operator.ASSIGN_REFERENCE);
-            Operators.define(Operator.CONDITION_AND);
-            Operators.define(Operator.CONDITION_OR);
-            Operators.define(Operator.CONDITION_EQUALS);
-            Operators.define(Operator.CONDITION_SMALLER);
-            Operators.define(Operator.CONDITION_SMALLER_EQUAL);
-            Operators.define(Operator.CONDITION_BIGGER);
-            Operators.define(Operator.CONDITION_BIGGER_EQUAL);
+
+            Operators.define(Operator.CALCULATE_ADD, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value(Convert.ToDecimal(a.getValue()) + Convert.ToDecimal(b.getValue()), ValueType.DECIMAL);
+                } else if(a.isType(ValueType.STRING) || b.isType(ValueType.STRING)) {
+                    return new Value(a.str() + b.str(), ValueType.STRING);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value(a.getValue<int>() + b.getValue<int>(), ValueType.INTEGER);
+                } else throw new OperationException("can not add values of types " + a.type.ToString() + " and " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CALCULATE_SUBTRACT, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value(Convert.ToDecimal(a.getValue()) - Convert.ToDecimal(b.getValue()), ValueType.DECIMAL);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value(a.getValue<int>() - b.getValue<int>(), ValueType.INTEGER);
+                } else throw new OperationException("can not substract values of types " + a.type.ToString() + " and " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CALCULATE_DIVIDE, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value(Convert.ToDecimal(a.getValue()) / Convert.ToDecimal(b.getValue()), ValueType.DECIMAL);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value(a.getValue<int>() / b.getValue<int>(), ValueType.INTEGER);
+                } else throw new OperationException("can not divide values of types " + a.type.ToString() + " and " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CALCULATE_MULTIPLY, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value(Convert.ToDecimal(a.getValue()) * Convert.ToDecimal(b.getValue()), ValueType.DECIMAL);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value(a.getValue<int>() * b.getValue<int>(), ValueType.INTEGER);
+                } else throw new OperationException("can not multiply values of types " + a.type.ToString() + " and " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CONDITION_AND, delegate (Value a, Value b) {
+                if (a.isType(ValueType.BOOLEAN) && b.isType(ValueType.BOOLEAN)) {
+                    return new Value(a.getValue<bool>() && b.getValue<bool>(), ValueType.BOOLEAN);
+                } else throw new OperationException("can not compare types for " + a.type.ToString() + " AND " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CONDITION_OR, delegate (Value a, Value b) {
+                if (a.isType(ValueType.BOOLEAN) && b.isType(ValueType.BOOLEAN)) {
+                    return new Value(a.getValue<bool>() || b.getValue<bool>(), ValueType.BOOLEAN);
+                } else throw new OperationException("can not compare types for " + a.type.ToString() + " OR " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CONDITION_EQUALS, delegate (Value a, Value b) {
+                return new Value(a.getValue().Equals(b.getValue()), ValueType.BOOLEAN);
+            });
+
+            Operators.define(Operator.CONDITION_SMALLER, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value((Convert.ToDecimal(a.getValue()) < Convert.ToDecimal(b.getValue())), ValueType.BOOLEAN);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value((a.getValue<int>() < b.getValue<int>()), ValueType.BOOLEAN);
+                } else throw new OperationException("can not compare types for " + a.type.ToString() + " SMALLER " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CONDITION_SMALLER_EQUAL, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value((Convert.ToDecimal(a.getValue()) <= Convert.ToDecimal(b.getValue())), ValueType.BOOLEAN);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value((a.getValue<int>() <= b.getValue<int>()), ValueType.BOOLEAN);
+                } else throw new OperationException("can not compare types for " + a.type.ToString() + " SMALLER_EQUAL " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CONDITION_BIGGER, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value((Convert.ToDecimal(a.getValue()) > Convert.ToDecimal(b.getValue())), ValueType.BOOLEAN);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value((a.getValue<int>() > b.getValue<int>()), ValueType.BOOLEAN);
+                } else throw new OperationException("can not compare types for " + a.type.ToString() + " BIGGER " + b.type.ToString());
+            });
+
+            Operators.define(Operator.CONDITION_BIGGER_EQUAL, delegate (Value a, Value b) {
+                if (a.isType(ValueType.DECIMAL) || b.isType(ValueType.DECIMAL)) {
+                    return new Value((Convert.ToDecimal(a.getValue()) >= Convert.ToDecimal(b.getValue())), ValueType.BOOLEAN);
+                } else if (a.isType(ValueType.INTEGER) && b.isType(ValueType.INTEGER)) {
+                    return new Value((a.getValue<int>() >= b.getValue<int>()), ValueType.BOOLEAN);
+                } else throw new OperationException("can not compare types for " + a.type.ToString() + " BIGGER_EQUAL " + b.type.ToString());
+            });
+
             Operators.define(Operator.COLLECTION_ADD);
             Operators.define(Operator.COLLECTION_REMOVE);
         }
